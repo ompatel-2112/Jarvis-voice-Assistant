740b090299ed66bd9ef8156c0246f9e246bb75ea
ompatel-2112 <ompatel43456@gmail.com>
2025-12-17 12:06:20 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "with you": "https://youtu.be/mZQH8CPQ-wo?si=T2N8VMnNAvaxQaeu",
+    "skyfall": "https://youtu.be/DeumyOzKqgI?si=Bd8J5hHrO5ZohZ3o",
+    "beliver": "https://youtu.be/W0DM5lcj6mw?si=KsG-s2U6m2X3AgL3",
+    "sundari": "https://youtu.be/WpBn9w-Js_c?si=8ovRLbHm_JaujlJ7" 
+}
